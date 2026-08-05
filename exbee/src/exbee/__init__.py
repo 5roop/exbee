961e79dc4b23ea744b316305d0bfe70f27652319
Peter Rupnik <peter.rupnik@ijs.si>
2026-08-05 09:04:33 +0200

    Make logging problems more verbose

--- exbee/src/exbee/__init__.py
+++ exbee/src/exbee/__init__.py
@@ -1,7 +1,7 @@
 from exbee.exb_parser import EXB
 from exbee.trs_parser import TRS
 
-__version__ = "2026.6.5"
+__version__ = "2026.8.5"
 
 
 def main() -> None:
--- exbee/src/exbee/exb_parser.py
+++ exbee/src/exbee/exb_parser.py
@@ -17,8 +17,16 @@ class EXB:
         # Check if trouble:
         if not self.test_tier_id_unique():
             logger.critical(f"Tiers have non-unique ids! Fix it!")
+            from collections import Counter
+
+            c = Counter([i.get("id") for i in self.doc.findall(".//tier")])
+            logger.critical(f"Non-unique display names: {[i for i in c if c[i] > 1]}")
         if not self.test_tier_display_name_unique():
             logger.critical(f"Tiers have non-unique display names! Fix it!")
+            from collections import Counter
+
+            c = Counter(self.get_tier_names())
+            logger.critical(f"Non-unique display names: {[i for i in c if c[i] > 1]}")
 
     def get_tier_names(self):
         tiers = self.doc.findall(".//tier")
@@ -53,7 +61,6 @@ class EXB:
                 ]
             )
         )
-
 
     def round_timeline(self, decimals=3) -> None:
         """Round all the timestamps to desired precision"""
